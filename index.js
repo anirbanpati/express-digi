63966c62c4d99145d1075c293712968874aaa7a1
Anirban Pati <anirbanpati55@gmail.com>
2024-08-28 11:01:20 +0530

    add to github

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+import 'dotenv/config';
 import express from 'express';
 
 
 const app = express();
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
